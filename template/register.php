03d18f2ed54d8bc779e5647918e4939151770241
Mikhail <mikhailnew84@gmail.com>
2017-01-30 14:28:00 +0300

    Небольшая правочка

--- template/register.php
+++ template/register.php
@@ -7,9 +7,9 @@
 <body>
 <form class="register center" action="/register" method="post">
     <div class="error"><?= $error; ?></div>
-    <input type="text" name="login"  placeholder="login" required value="<?= $_POST["login"] ?? ""; ?>"><br>
+    <input type="text" name="login"  placeholder="login" required value="<?= $_POST["login"] ? htmlspecialchars($_POST["login"]): ""; ?>"><br>
     <input type="password" name="password" placeholder="Пароль"  required><br>
-    <input type="text" name="birthday" placeholder="Дата рождения (дд.мм.гггг)" required value="<?= $_POST["birthday"] ?? ""; ?>">
+    <input type="text" name="birthday" placeholder="Дата рождения (дд.мм.гггг)" required value="<?= $_POST["birthday"] ? htmlspecialchars($_POST["birthday"]): ""; ?>">
     <button name="register">Зарегистрироваться</button>
 </form>
 </body>
